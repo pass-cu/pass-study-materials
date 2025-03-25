bc603f2c9037dd4c8299ff9e2887eac0ba8b9afe
Andrea N. <nicholaswaworuntu@gmail.com>
2025-03-25 01:24:11 -0400

    Week 11

---
+++ W25_COMP_1005-1405/practice-problems/Week 11/main.py
@@ -0,0 +1,23 @@
+'''
+Week 11
+
+Put your code here for the activities OR play around with the dictionaries.
+
+The following coding problems refer to the dictionary in words.py file. By importing this below, we can access it.
+'''
+
+'''
+ACTIVITY ONE
+
+Print out the dictionary in a nice format such as below!
+
+WORD            TYPE            DEFINITION
+ubiquitous      adjective       Present, appearing, or found everywhere.
+meticulous      adjective       Showing great attention to detail; very careful and precise.
+...and so on...
+'''
+
+import words
+
+def print_dictionary() -> None:
+    data = words.data
---
+++ W25_COMP_1005-1405/practice-problems/Week 11/words.py
@@ -0,0 +1,53 @@
+# A dictionary of words with their type and definitions
+data = [
+    {
+        "word": "serendipity",
+        "type": "noun",
+        "def": "The occurrence of events by chance in a happy or beneficial way."
+    },
+    {
+        "word": "elucidate",
+        "type": "verb",
+        "def": "To make something clear; to explain."
+    },
+    {
+        "word": "ephemeral",
+        "type": "adjective",
+        "def": "Lasting for a very short time."
+    },
+    {
+        "word": "facetious",
+        "type": "adjective",
+        "def": "Treating serious issues with deliberately inappropriate humor."
+    },
+    {
+        "word": "ubiquitous",
+        "type": "adjective",
+        "def": "Present, appearing, or found everywhere."
+    },
+    {
+        "word": "perspicacious",
+        "type": "adjective",
+        "def": "Having a ready insight into and understanding of things."
+    },
+    {
+        "word": "meticulous",
+        "type": "adjective",
+        "def": "Showing great attention to detail; very careful and precise."
+    },
+    {
+        "word": "tenacious",
+        "type": "adjective",
+        "def": "Tending to keep a firm hold on something; determined."
+    },
+    {
+        "word": "quintessential",
+        "type": "adjective",
+        "def": "Representing the most perfect or typical example of something."
+    },
+    {
+        "word": "benevolent",
+        "type": "adjective",
+        "def": "Well-meaning and kindly."
+    }
+]
